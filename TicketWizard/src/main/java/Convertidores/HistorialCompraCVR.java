88ae3cea60d1bf54f7e433587ed305517353d8c6
levin2403 <kevin.sanchez240798@potros.itson.edu.mx>
2024-09-26 00:42:55 -0700

    agregar entidades, pojo y convertidor
    agregue las entidades para el historial de compras (POJO, DTO) y el convertidor de la misma

--- TicketWizard/src/main/java/Convertidores/HistorialCompraCVR.java
+++ TicketWizard/src/main/java/Convertidores/HistorialCompraCVR.java
@@ -4,10 +4,61 @@
  */
 package Convertidores;
 
+import DTOs.HistorialCompraDTO;
+import Entidades.HistorialCompra;
+import Entidades.Tipo_boleto;
+import Excepciones.BOException;
+
 /**
  *
  * @author skevi
  */
 public class HistorialCompraCVR {
     
+    private final PersonaCVR personaCVR;
+    private final BoletoCVR boletoCVR;
+
+    public HistorialCompraCVR() {
+        this.personaCVR = new PersonaCVR();
+        this.boletoCVR = new BoletoCVR();
+    }
+
+    
+    public HistorialCompraDTO toDTO(HistorialCompra hc) throws BOException{
+        if (hc == null) {
+            return null;
+        }
+        
+        try{
+        return new HistorialCompraDTO(
+            String.valueOf(hc.getId()),
+            personaCVR.convertirADTO(hc.getPersona()),
+            boletoCVR.toDTO(hc.getBoleto()),
+            hc.getFecha_compra(),
+            hc.getHora_compra(),
+            hc.getTipo_compra().name()
+        );
+        }catch(BOException ex){
+            throw new BOException(ex.getMessage());
+        }
+    }
+    
+    public HistorialCompra toDTO(HistorialCompraDTO hcDTO) throws BOException{
+        if (hcDTO == null) {
+            return null;
+        }
+        
+        try{
+        return new HistorialCompra(
+            Integer.parseInt(hcDTO.getId()),
+            personaCVR.convertirAEntidad(hcDTO.getPersona()),
+            boletoCVR.toEntity(hcDTO.getBoleto()),
+            hcDTO.getFecha_compra(),
+            hcDTO.getHora_compra(),
+            Tipo_boleto.valueOf(hcDTO.getTipo_compra())
+        );
+        }catch(BOException ex){
+            throw new BOException(ex.getMessage());
+        }
+    }
 }
--- TicketWizard/src/main/java/DTOs/HistorialCompraDTO.java
+++ TicketWizard/src/main/java/DTOs/HistorialCompraDTO.java
@@ -4,10 +4,90 @@
  */
 package DTOs;
 
+import java.sql.Date;
+import java.sql.Time;
+
 /**
  *
  * @author skevi
  */
 public class HistorialCompraDTO {
     
+    private String id;
+    private PersonaDTO persona; //persona que compro el boleto
+    private BoletoDTO boleto; //boleto que fue comprado
+    private Date fecha_compra;
+    private Time hora_compra;
+    private String tipo_compra;
+
+    public HistorialCompraDTO() {
+    }
+
+    public HistorialCompraDTO(PersonaDTO persona, BoletoDTO boleto, 
+            Date fecha_compra, Time hora_compra, String tipo_compra) {
+        this.persona = persona;
+        this.boleto = boleto;
+        this.fecha_compra = fecha_compra;
+        this.hora_compra = hora_compra;
+        this.tipo_compra = tipo_compra;
+    }
+
+    public HistorialCompraDTO(String id, PersonaDTO persona, BoletoDTO boleto, 
+            Date fecha_compra, Time hora_compra, String tipo_compra) {
+        this.id = id;
+        this.persona = persona;
+        this.boleto = boleto;
+        this.fecha_compra = fecha_compra;
+        this.hora_compra = hora_compra;
+        this.tipo_compra = tipo_compra;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public PersonaDTO getPersona() {
+        return persona;
+    }
+
+    public void setPersona(PersonaDTO persona) {
+        this.persona = persona;
+    }
+
+    public BoletoDTO getBoleto() {
+        return boleto;
+    }
+
+    public void setBoleto(BoletoDTO boleto) {
+        this.boleto = boleto;
+    }
+
+    public Date getFecha_compra() {
+        return fecha_compra;
+    }
+
+    public void setFecha_compra(Date fecha_compra) {
+        this.fecha_compra = fecha_compra;
+    }
+
+    public Time getHora_compra() {
+        return hora_compra;
+    }
+
+    public void setHora_compra(Time hora_compra) {
+        this.hora_compra = hora_compra;
+    }
+
+    public String getTipo_compra() {
+        return tipo_compra;
+    }
+
+    public void setTipo_compra(String tipo_compra) {
+        this.tipo_compra = tipo_compra;
+    }
+             
 }
